66ead63c2f67ee2dbc7ebd6700ca61e00f1b0456
Lee Tucker <lee.c.tucker@gmail.com>
2018-02-12 18:58:08 -0500

    Updates and bug fixes.

--- generate_secrets.sh
+++ generate_secrets.sh
@@ -2,13 +2,14 @@
 
 # List all available IP addresses, but set the eth0 address as default
 
-echo "AVAILABLE IP ADDRESSES\n----------------------"
+echo -e "AVAILABLE IP ADDRESSES\n----------------------"
 ifconfig | awk -v RS="\n\n" '{ for (i=1; i<=NF; i++) if ($i == "inet" && $(i+1) ~ /^addr:/) address = substr($(i+1), 6); if (address != "127.0.0.1") printf "%s\t%s\n", $1, address }'
 DEFAULT_IP="$(ifconfig eth0 | grep "inet addr" | cut -d ':' -f 2 | cut -d ' ' -f 1)"
 
-read -p "\nEnter desired Swarm IP address [${DEFAULT_IP}]: " IP || IP="${DEFAULT_IP}"
+read -p "Enter desired Swarm IP address [${DEFAULT_IP}]: " IP
+IP=${IP:-$DEFAULT_IP}
 
-read -p "\nEnter your server's domain and subdomain name(s), separated by spaces. [${DEFAULT_IP}]: "
+read -p "Enter your server's domain and subdomain name(s), separated by spaces. [${DEFAULT_IP}]: "
 
 # Initialize the Docker Swarm
 docker swarm init --advertise-addr $IP
@@ -17,32 +18,32 @@ docker swarm init --advertise-addr $IP
 POSTRES_USER=$(openssl rand -base64 20) 
 POSTGRES_PASSWORD=$(openssl rand -base64 20)
 
-docker secret create postgres_user $POSTRES_USER
-docker secret create postgres_password $POSTGRES_PASSWORD
-docker secret create postgres_url "postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@localhost:5432/danceschool_web"
+echo "${POSTGRES_USER}" | docker secret create postgres_user -
+echo "${POSTGRES_PASSWORD}" | docker secret create postgres_password -
+echo "postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@localhost:5432/danceschool_web" | docker secret create postgres_url -
 
 # Generate a Django secret key
 openssl rand -base64 30 | docker secret create django_secret_key -
 
-SSL_SET=false
+SSL_SET="false"
 
 # Require an SSL certificate either by having a file provided, or by generating one using OpenSSL
-while [! $SSL_SET ]
+echo -e '\n\nPlease select a source for an SSL certificate:\n'
 
-
-PS3='\nPlease select a source for an SSL certificate: '
 options=("Provide an SSL Certificate" "Generate Using OpenSSL" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
         "Provide an SSL Certificate")
-            read -p "\nEnter location of SSL certificate (usually *.crt): " PROVIDED_CERT_PATH
+            echo -e "\n"
+
+            read -p "Enter location of SSL certificate (usually *.crt): " PROVIDED_CERT_PATH
             if [ ! -f PROVIDED_CERT_PATH ]; then
                 echo "File not found!"
                 continue
             fi
 
-            read -p "\nEnter location of SSL certificate key (usually *.key): " PROVIDED_CERT_KEY_PATH
+            read -p "Enter location of SSL certificate key (usually *.key): " PROVIDED_CERT_KEY_PATH
             if [ ! -f PROVIDED_CERT_KEY_PATH ]; then
                 echo "File not found!"
                 continue
@@ -53,10 +54,11 @@ do
             docker secret create site.crt $PROVIDED_CERT_PATH
 
             # Ready to break out of the loop
-            SSL_SET=true
+            break
+
             ;;
         "Generate Using OpenSSL")
-            echo "Generating Certificate using OpenSSL"
+            echo -e "\nGenerating Certificate using OpenSSL\n\n"
             mkdir ./openssl
 
             # This command will provide the usual prompts for information needed to generate the certificate
@@ -68,12 +70,16 @@ do
             rm -r ./openssl
 
             # Ready to break out of the loop
-            SSL_SET=true
+            break
+
             ;;
         "Quit")
-            echo "\nUnable to proceed with Docker setup."
+            echo -e "\nUnable to proceed with Docker setup."
             break
             ;;
         *) echo invalid option;;
     esac
 done
+
+docker build -t danceschool_nginx ./docker/nginx
+docker build -t danceschool_web ./docker/web
