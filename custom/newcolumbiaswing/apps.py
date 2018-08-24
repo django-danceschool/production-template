1ddeac0114555275d9fe3e531e6220fdb3f1a291
Lee Tucker <lee.c.tucker@gmail.com>
2018-08-24 17:00:40 -0400

    Switched to CMS placeholders with plugin defaults.

--- custom/newcolumbiaswing/apps.py
+++ custom/newcolumbiaswing/apps.py
@@ -6,3 +6,40 @@ from django.utils.translation import ugettext_lazy as _
 class NewColumbiaAppConfig(AppConfig):
     name = 'newcolumbiaswing'
     verbose_name = _('New Columbia Swing Functions')
+
+    def ready(self):
+        ''' 
+        Update the default placeholder configuration to set the default splash image and default title.
+        '''
+        from django.conf import settings
+
+        if not isinstance(getattr(settings,'CMS_PLACEHOLDER_CONF',None),dict):
+            settings.CMS_PLACEHOLDER_CONF = {}
+
+        settings.CMS_PLACEHOLDER_CONF.update({
+            'splash_image': {
+                'name': 'New Columbia Swing Background Image',
+                'limits': {
+                    'global': 1,
+                },
+                'default_plugins':[
+                    {
+                        'plugin_type': 'TextPlugin',
+                        'values':{
+                            'body': 'http://placehold.it/1920x400',
+                        },
+                    },
+                ]
+            },
+            'splash_title': {
+                'name': 'New Columbia Swing Front Page Title',
+                'default_plugins':[
+                    {
+                        'plugin_type': 'TextPlugin',
+                        'values': {
+                            'body': 'New Columbia Swing',
+                        }
+                    }
+                ]
+            }
+        })
