a9ddc018d1bcea207e07b9120888b746afa412c6
Lee Tucker <lee.c.tucker@gmail.com>
2018-09-17 22:19:22 -0400

    Fixed Nginx media handling.

--- school/urls.py
+++ school/urls.py
@@ -13,12 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += [
     # Include your own app's URLs first to override default app URLs
