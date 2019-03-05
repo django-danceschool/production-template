6a1cbb91c5218c4e11585dde03657490c70ff4f8
Lee Tucker <lee.c.tucker@gmail.com>
2019-03-04 22:58:22 -0500

    Added post-Swing 1 email code (still requires testing).

--- custom/newcolumbiaswing/apps.py
+++ custom/newcolumbiaswing/apps.py
@@ -2,7 +2,31 @@
 from django.apps import AppConfig
 from django.utils.translation import ugettext_lazy as _
 
+from danceschool.core.utils.sys import isPreliminaryRun
+
 
 class NewColumbiaAppConfig(AppConfig):
     name = 'newcolumbiaswing'
     verbose_name = _('New Columbia Swing Functions')
+
+    def ready(self):
+        from . import handlers
+        from django.db import connection
+        from danceschool.core.constants import getConstant, updateConstant
+
+        if 'core_emailtemplate' in connection.introspection.table_names() and not isPreliminaryRun():
+            from danceschool.core.models import EmailTemplate, get_defaultEmailName, get_defaultEmailFrom
+
+            if (getConstant('promotions__lindy1TemplateID') or 0) <= 0:
+                new_template, created = EmailTemplate.objects.get_or_create(
+                    name=_('Post-Swing 1 Email Promotion With Vouchers'),
+                    defaults={
+                        'subject': _('Congratulations on Completing Swing 1!'),
+                        'content': '',
+                        'defaultFromAddress': get_defaultEmailFrom,
+                        'defaultFromName': get_defaultEmailName,
+                        'defaultCC': '',
+                        'hideFromForm': True,}
+                )
+                # Update constant and fail silently
+                updateConstant('promotions__lindy1TemplateID',new_template.id,True)
--- custom/newcolumbiaswing/tasks.py
+++ custom/newcolumbiaswing/tasks.py
@@ -1,11 +1,20 @@
 from django.conf import settings
+from django.db.models import Count, Case, When, IntegerField, Q, F
+from django.utils.translation import ugettext_lazy as _
 from django.utils import timezone
 
 from huey import crontab
-from huey.contrib.djhuey import periodic_task, db_task
+from huey.contrib.djhuey import periodic_task, db_task, db_periodic_task
+from datetime import timedelta
 from dateutil.relativedelta import relativedelta
 import logging
 from mailsnake import MailSnake
+from random import shuffle
+
+from danceschool.core.models import Series, Event, EmailTemplate, Customer, DanceTypeLevel
+from danceschool.core.constants import getConstant
+from danceschool.core.mixins import EmailRecipientMixin
+from danceschool.vouchers.models import Voucher
 
 
 # Define logger for this file
@@ -84,3 +93,150 @@ def backupDatabaseSFTP():
             transport.close()
     else:
         logger.info('Backup settings not provided, no SFTP backup performed.')
+
+
+@db_periodic_task(crontab(hour='13',minute='30'))
+def emailRecentNewStudents():
+    '''
+    This program identifies all new students who have recently completed
+    Lindy 1 classes (or other beginner classes) and sends them a custom
+    email giving them a discount.
+    '''
+    enabled = getConstant('promotions__enableLindy1PromotionEmail')
+    if not enabled:
+        return
+
+    logger.info('Sending Lindy 1 completion promotional emails.')
+
+    # These levels are used to determine the classes to which the vouchers apply.
+    try:
+        lindy1Level = DanceTypeLevel.objects.get(danceType__name='Lindy Hop',name='Level 1')
+    except:
+        logger.error('Lindy 1 Dance type not found -- vouchers could not be created.')
+        return
+
+    # This is the email template used to send the promotional email
+    template = EmailTemplate.objects.get(id=getConstant('promotions__lindy1TemplateID'))
+
+    # This dict holds the basic voucher details that will be filled in.
+    # Change these param
+    vouchers = {
+        'Lindy_1': {
+            'name': 'Swing 1',
+            'amount': 35,
+            'prefix': 'SWING1_RETAKE_',
+            'category': getConstant('vouchers__emailPromoCategory'),
+            'expirationDate': timezone.now() + relativedelta(months=6),
+            'danceTypeLevel': lindy1Level,
+        },
+        'Lindy_2': {
+            'name': 'Swing 2',
+            'amount': 15,
+            'prefix': 'SWING2_CONTINUE_',
+            'category': getConstant('vouchers__emailPromoCategory'),
+            'expirationDate': timezone.now() + relativedelta(months=3),
+        }
+    }
+
+    # Identify the recent series whose students to contact.
+    # - Ended in the last 30 days
+    # - Is Lindy Hop
+    # - Is of level 1
+    recent_series = Series.objects.filter(
+        endTime__lte=timezone.now(),
+        endTime__gte=timezone.now() - timedelta(days=30),
+        classDescription__danceTypeLevel=lindy1Level,
+    )
+
+    if recent_series.count() == 0:
+        logger.info('No eligible recent series found.  Aborting.')
+        return
+
+    # Identify the students to contact.
+    # - in the applicable series
+    # - Didn't take a beginner Lindy course other than in one of the applicable Lindy 1 series
+    # - Haven't already received the promotion
+    possible_customers = Customer.objects.annotate(
+        thislindyonecount=Count(Case(When(Q(eventregistration__event__series__classDescription__danceTypeLevel=lindy1Level) & Q(eventregistration__event__series__in=recent_series),then=1),output_field=IntegerField())),
+        alllindyonecount=Count(Case(When(eventregistration__event__series__classDescription__danceTypeLevel=lindy1Level, then=1),output_field=IntegerField())),
+    ).annotate(
+        otherlindyonecount=F('alllindyonecount') - F('thislindyonecount')
+    ).filter(
+        thislindyonecount__gt=0,
+        otherlindyonecount=0,
+    ).select_related('user').distinct()
+
+    uncontacted_customers = [x for x in possible_customers if not isinstance(x.data, dict) or not x.data.get('lindy1PromoEmailSent')]
+
+    if len(uncontacted_customers) == 0:
+        logger.info('No eligible customers found.  Aborting.')
+        return
+
+    # The list of upcoming series includes Swing 1, Swing 2, and anything
+    # labeled "All Levels"
+    upcoming_series = Series.objects.filter(
+        startTime__gte=timezone.now(),
+        registrationOpen=True,
+        status__in=[Event.RegStatus.enabled, Event.RegStatus.heldOpen],
+        classDescription__danceTypeLevel__name__in=[
+            'Level 1','Level 2','All Levels','Solo jazz - Level 1',
+        ],
+    ).order_by('startTime')
+
+    for customer in uncontacted_customers:
+        logger.debug('Preparing to send promotional email to customer %s.' % customer.id)
+
+        for voucherKey,voucherInfo in vouchers.items():
+            newVoucher = Voucher.create_new_code(
+                prefix=voucherInfo.get('prefix'),
+                name=_('Student Retention %s Discount for %s' % (voucherInfo['name'],customer.fullName)),
+                category=voucherInfo['category'],
+                originalAmount=voucherInfo['amount'],
+                singleUse=True,
+                forFirstTimeCustomersOnly=False,
+                expirationDate=voucherInfo['expirationDate'],
+            )
+            voucherInfo['code'] = newVoucher.voucherId
+
+            if voucherInfo.get('danceTypeLevel'):
+                newVoucher.dancetypevoucher_set.create(danceTypeLevel=voucherInfo['danceTypeLevel'])
+
+        # The email will be signed by the instructors of the series that ended
+        instructors = customer.eventregistration_set.filter(
+            event__series__classDescription__danceTypeLevel=lindy1Level,
+        ).order_by('event__endTime').first().event.teachers
+        shuffle(instructors)
+
+        # For convenience, do this here rather than in the template
+        teacher_first_names = ', '.join([x.firstName for x in instructors])
+
+        if not template.defaultFromAddress or not template.content:
+            logger.warning('Cannot send email to customer %s because template is not yet configured.' % customer.id)
+        else:
+            emailMixin = EmailRecipientMixin()
+
+            emailMixin.email_recipient(
+                template.subject,
+                template.content,
+                html_message=template.html_content,
+                send_html=True,
+                from_address=template.defaultFromAddress,
+                from_name=template.defaultFromName,
+                cc=template.defaultCC,
+                to=customer.email,
+                vouchers=vouchers,
+                instructors=instructors,
+                teacher_first_names=teacher_first_names,
+                upcoming_series=upcoming_series,
+                recipient_name=customer.fullName,
+            )
+
+        if not customer.data or not isinstance(customer.data,dict):
+            customer.data = {}
+        customer.data['lindy1PromoEmailSent'] = timezone.now().strftime('%Y-%m-%d %H:%M:%S')
+
+        # Save out the customer to mark that the email was sent.
+        customer.save()
+        logger.info('Sent Lindy 1 Completion promotional email to customer %s' % customer.id)
+
+    logger.info('Sending of Lindy 1 Completion promotion emails complete.')
