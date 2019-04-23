f0a6f00bfb80f323d5648b274faf9852af3e764e
Lee Tucker <lee.c.tucker@gmail.com>
2019-04-22 20:48:26 -0400

    Removed accidentally-included New Columbia Swing app.

--- custom/newcolumbiaswing/constants.py
+++
@@ -1,8 +0,0 @@
-HOW_HEARD_CHOICES = [
-    ('','------'),
-    ('Previous Student','I\'ve Taken Classes Before'),
-    ('Facebook','Facebook'),
-    ('Flyers','Flyers/Cards'),
-    ('Friend','Another Student'),
-    ('Other','Other'),
-]
